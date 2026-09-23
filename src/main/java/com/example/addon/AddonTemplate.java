9028beece6d3ecd04615a347b26b48f1b50ac9cd
defgodqe-arch <defgodqe@gmail.com>
2026-09-23 18:02:27 -0500

    Register Mace Elytra module and point addon metadata to defgodqe-arch/meteor

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,6 +2,7 @@ package com.example.addon;
 
 import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
+import com.example.addon.modules.MaceElytra;
 import com.example.addon.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
@@ -24,6 +25,7 @@ public class AddonTemplate extends MeteorAddon {
 
         // Modules
         Modules.get().add(new ModuleExample());
+        Modules.get().add(new MaceElytra());
 
         // Commands
         Commands.add(new CommandExample());
@@ -44,6 +46,6 @@ public class AddonTemplate extends MeteorAddon {
 
     @Override
     public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
+        return new GithubRepo("defgodqe-arch", "meteor");
     }
 }
